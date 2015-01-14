b006199a29e9389a0cdef4adecfeed9599170c01
cnup2732 <cnup2732@bths.edu>
2015-01-14 18:20:29 -0500

    did most of the base

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
+  
   get '/nationality' do
     erb :nationality
   end
@@ -13,12 +14,42 @@ class MyApp < Sinatra::Base
   get '/chineseboy' do
     erb :chineseboy
   end
+  
+  get '/chinesegirl' do
+    erb :chinesegirl
+  end
+
+  get '/koreanboy' do
+    erb :koreanboy
+  end
+
+  get '/koreangirl' do
+    erb :koreangirl
+  end
+
+  get '/indianboy' do
+    erb :indianboy
+  end
+
+  get '/indiangirl' do
+    erb :indiangirl
+  end
 
   post '/nationality' do
     puts params
     if params[:gender] == "Boy" && params[:nationality] == "Chinese"
       redirect '/chineseboy'
+    elsif params[:gender] == "Girl" && params[:nationality] == "Chinese"
+      redirect '/chinesegirl'
+    elsif params[:gender] == "Boy" && params[:nationality] == "Korean"
+      redirect '/koreanboy'
+    elsif params[:gender] == "Girl" && params[:nationality] == "Korean"
+      redirect '/koreangirl'
+    elsif params[:gender] == "Boy" && params[:nationality] == "Indian"
+      redirect '/indianboy'
+    else params[:gender] == "Girl" && params[:nationality] == "Indian"
+      redirect '/indiangirl'
     end
   end
-
 end
+
