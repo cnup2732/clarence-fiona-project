3dd6fe28cc2874c27acf1dd0a52f59e92cebfa06
cnup2732 <cnup2732@bths.edu>
2015-01-14 16:42:20 -0500

    Created chinese boy

--- app.rb
+++ app.rb
@@ -6,5 +6,19 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
+  get '/nationality' do
+    erb :nationality
+  end
+
+  get '/chineseboy' do
+    erb :chineseboy
+  end
+
+  post '/nationality' do
+    puts params
+    if params[:gender] == "Boy" && params[:nationality] == "Chinese"
+      redirect '/chineseboy'
+    end
+  end
 
 end
